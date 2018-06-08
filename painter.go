d14de1c41475c89e118a7ffb7c53c39463056e80
hellozee <hellozee@disroot.org>
2018-06-08 17:08:55 +0530

    Added Code for drawing backgrounds

---
+++ painter.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/signintech/gopdf"
+)
+
+//GeneratePoster  Function to generate a poster based on the given details
+func GeneratePoster(posterDetails Document) {
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: gopdf.Rect{W: 595, H: 842}}) //A4
+	pdf.AddPage()
+
+	//Draw Background
+	rect := gopdf.Rect{W: 595, H: 842}
+	err := pdf.Image(posterDetails.Background, 0, 0, &rect)
+	must(err)
+
+	pdf.WritePdf("Poster.pdf")
+}
